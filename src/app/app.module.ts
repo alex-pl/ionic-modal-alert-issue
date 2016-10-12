92878b412e84c064e7253ca80babcd861801d329
Alexander Ploner <alex@neatnet.de>
2016-10-12 18:53:22 +0200

    Removed unnecessary things.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { ListPage } from '../pages/list/list';
+import { Page } from '../pages/page/page';
 
 @NgModule({
   declarations: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    Page
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -18,9 +14,7 @@ import { ListPage } from '../pages/list/list';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    Page
   ],
   providers: []
 })
--- src/pages/page/page.ts
+++ src/pages/page/page.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  templateUrl: 'hello-ionic.html'
+  templateUrl: 'page.html'
 })
-export class HelloIonicPage {
+export class Page {
   constructor() {
 
   }
